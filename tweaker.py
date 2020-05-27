b9072cfd3eeefd118dd57406a1f23a247a126ee7
guptanikhil719 <62532605+guptanikhil719@users.noreply.github.com>
2020-05-27 13:53:05 +0530

    5

--- tweaker.py
+++ tweaker.py
@@ -1,7 +1,7 @@
 accuracy_file = open('/root/mlopsactivity3/accuracy.txt','r')
 input_file = open('/root/mlopsactivity3/input.txt','r')
 data_file = open('/root/mlopsactivity3/data.txt','r')
-
+display_matter=open('root/mlopsactivity3/display_matter.html','r')
 
 
 new_accuracy = float(accuracy_file.read()) 
@@ -27,3 +27,17 @@ input_file.close()
 input_file = open('/root/mlopsactivity3/input.txt','w')
 input_file.write(str(epochs))
 input_file.close()
+display_matter=open('root/mlopsactivity3/displaymatter.html','w')
+
+display_matter.write('\nLayer -1')
+display_matter.write('\nNumber Of Filters : 20')
+display_matter.write('\nFilter Size : 5')
+display_matter.write('\nPool Size : 2')
+
+display_matter.write('\nLayer -2')
+display_matter.write('\nNumber Of Filters : 50')
+display_matter.write('\nFilter Size : 5')
+display_matter.write('\nPool Size : 2')
+display_matter.write('\nEpochs :'+ str(epochs))
+display_matter.close()
+
